b1ec54fc9257538f5b76064fe3d1aca71718fba6
Uli Hansen <ulrich.hansen@acentic.com>
2017-04-27 10:52:18 +0200

    save

---
+++ core/src/main/java/hanulhan/jms/spring/reqreply/util/JMSReqErrorHandler.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hanulhan.jms.spring.reqreply.util;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.util.ErrorHandler;
+
+/**
+ *
+ * @author uhansen
+ */
+public class JMSReqErrorHandler implements ErrorHandler    {
+
+    private static final Logger LOGGER = Logger.getLogger(JMSReqErrorHandler.class);
+    
+    /**
+     *
+     * @param thrwbl
+     */
+    @Override
+    public void handleError(Throwable thrwbl) {
+        LOGGER.log(Level.ERROR, "Error in Req-EventListener\n" + thrwbl);
+    }
+    
+}
